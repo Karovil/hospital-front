edc52a3b316b8c0910626ee4c62009976cd2af73
ANGIE CAROLINA PAREJA VILLA <angiepareja1112998@correo.itm.edu.co>
2024-05-23 19:52:13 -0500

    Campos obligatorios al crear usuarios y asiganr citas para el componente admin

--- src/app/admin-home/admin-home.component.ts
+++ src/app/admin-home/admin-home.component.ts
@@ -57,17 +57,60 @@ export class AdminHomeComponent {
     this.newUser = { role: selectedRole, name: '', age: undefined, rh: '', specialization: '', email: '', password: '' };
   }
 
-  createUser() {
+  createUser() {  const requiredFieldsFilled = (
+    this.newUser.name &&
+    this.newUser.email &&
+    this.newUser.password &&
+    ((this.newUser.role === 'patient' && this.newUser.age && this.newUser.rh) ||
+    (this.newUser.role === 'doctor' && this.newUser.specialization))
+  );
+
+  // Si todos los campos obligatorios están llenos, se crea el usuario
+  if (requiredFieldsFilled) {
     const newUser = { ...this.newUser };
     this.users.push(newUser);
-    this.newUser = { role: 'patient', name: '', age: undefined, rh: '', specialization: '', email: '', password: '' };
+    this.newUser = {
+      role: 'patient',
+      name: '',
+      age: undefined,
+      rh: '',
+      specialization: '',
+      email: '',
+      password: ''
+    };
+  } else {
+    // Si algún campo obligatorio está vacío, se muestra una alerta
+    alert('Please fill in all required fields.');
   }
+}
 
   assignAppointment() {
+     // Verificar si todos los campos obligatorios están llenos
+  const requiredFieldsFilled = (
+    this.newAppointment.doctorId &&
+    this.newAppointment.date &&
+    this.newAppointment.consultationRoom &&
+    this.newAppointment.diagnosis
+  );
+
+  // Si todos los campos obligatorios están llenos, se asigna la cita
+  if (requiredFieldsFilled) {
     const newAppointment = { ...this.newAppointment };
     this.assignedAppointments.push(newAppointment);
-    this.newAppointment = { doctorId: 0, patientId: '', doctor: '', patient: '', date: '', consultationRoom: '', diagnosis: '' };
+    this.newAppointment = {
+      doctorId: 0,
+      patientId: '',
+      doctor: '',
+      patient: '',
+      date: '',
+      consultationRoom: '',
+      diagnosis: ''
+    };
+  } else {
+    // Si algún campo obligatorio está vacío, se muestra una alerta
+    alert('Please fill in all required fields.');
   }
+}
 
   editAppointment(appointment: Appointment) {
     this.editingAppointment = { ...appointment };
